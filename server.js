2f4711449a6fe48c5f26a6ce6458053f45425aca
Michael Peterson <WebDev@ctoms.ca>
2019-06-27 14:30:35 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const express = require('express');
+const {PORT} = require('./config');
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
+const app = express();
+app.use(jsonParser);
+let server;
+
+function runServer(port = PORT){
+
+	return new Promise((resolve,reject) => {
+		server = app.listen(port, () => {
+			console.log('listening on port ',port)
+					
+		})
+		
+		.on('error',err => {
+			reject(err);
+		})
+	})
+	.catch(err => {
+		console.log("error ",err);
+	});
+}
+
+function closeServer(){
+	return new Promise((resolve,reject) => {
+		console.log("closing server");
+		server.close(err => {
+			if(err){
+				return reject(err);
+			}
+			resolve();
+		});
+	});
+}
+
+if (require.main === module){
+	runServer().catch(err => console.log(err));
+}
+
+module.exports = { app, runServer, closeServer };
